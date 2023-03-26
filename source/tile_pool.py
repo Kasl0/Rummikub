78a7db6eb3e9c3edec7512a193bd5a3736bfb383
Gracjan <filipek.gracjan@o2.pl>
2023-03-26 22:31:02 +0200

    Changed some attributes and methods to private/protected

--- source/tile_pool.py
+++ source/tile_pool.py
@@ -6,12 +6,12 @@ from random import randint
 class TilePool:
     """Pool of tiles which can be drawn by the player"""
     def __init__(self, unavailable_tiles: list[Tile] = []):
-        self.tiles = []
+        self.__tiles = []
         for color in Color:
             for value in range(1, 14):
                 # each tile has to copies in starting pool
-                self.tiles.append(Tile(value, color))
-                self.tiles.append(Tile(value, color))
+                self.__tiles.append(Tile(value, color))
+                self.__tiles.append(Tile(value, color))
 
         for tile in unavailable_tiles:
             self.remove_tile(tile)
@@ -19,7 +19,7 @@ class TilePool:
     def if_tile_in_pool(self, tile: Tile):
         """Check if given tile is in the pool"""
 
-        for rack_tile in self.tiles:
+        for rack_tile in self.__tiles:
             if rack_tile is tile:
                 return True
         return False
@@ -29,14 +29,14 @@ class TilePool:
 
         Returns drawn tile or None if there's no more tiles in the pool"""
 
-        if len(self.tiles) == 0:
+        if len(self.__tiles) == 0:
             raise Exception("No more tiles in the tile pool")
 
-        rnd_num = randint(0, len(self.tiles) - 1)
-        tile_to_return = self.tiles[rnd_num]
+        rnd_num = randint(0, len(self.__tiles) - 1)
+        tile_to_return = self.__tiles[rnd_num]
 
         self.remove_tile(tile_to_return)
         return tile_to_return
 
     def remove_tile(self, tile: Tile):
-        self.tiles.remove(tile)
+        self.__tiles.remove(tile)
