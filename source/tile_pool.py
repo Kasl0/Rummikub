91262009f8e735aa541ee04ef0643e69386dc364
Gracjan <filipek.gracjan@o2.pl>
2023-03-26 18:49:56 +0200

    Extended __init__ of TilePool

--- source/tile_pool.py
+++ source/tile_pool.py
@@ -2,14 +2,18 @@ from color import Color
 from tile import Tile
 from random import randint
 
+
 class TilePool:
-    def __init__(self):
+    def __init__(self, unavailable_tiles: list[Tile] = []):
         self.tiles = []
         for color in Color:
             for value in range(1, 14):
                 # each tile has to copies in starting pool
                 self.tiles.append(Tile(value, color))
                 self.tiles.append(Tile(value, color))
+
+        for tile in unavailable_tiles:
+            self.remove_tile(tile)
 
     # check if given tile is in the pool
     def if_tile_in_pool(self, tile: Tile):
@@ -26,5 +30,5 @@ class TilePool:
         self.remove_tile(tile_to_return)
         return tile_to_return
 
-    def _remove_tile(self, tile: Tile):
+    def remove_tile(self, tile: Tile):
         self.tiles.remove(tile)
